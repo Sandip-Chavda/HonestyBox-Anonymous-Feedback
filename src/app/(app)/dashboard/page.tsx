1825be294eed2e4d1cc37138a903108f2aba7f09
Sandip-Chavda <chavdas766@gmail.com>
2024-08-02 12:28:07 +0530

    Dashboard added with recieved messages and public link share

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,181 @@
-import React from "react";
+"use client";
+
+import { Message } from "@/model/User";
+import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React, { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { User } from "next-auth";
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { Separator } from "@/components/ui/separator";
+import { Loader2, RefreshCcw } from "lucide-react";
+import MessageCard from "@/components/message-card/MessageCard";
 
 const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false);
+
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId));
+  };
+
+  const { data: session } = useSession();
+
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema),
+  });
+
+  const { watch, register, setValue } = form;
+
+  const acceptMessages = watch("acceptMessages");
+
+  const fetchAcceptMessage = useCallback(async () => {
+    setIsSwitchLoading(true);
+
+    try {
+      const response = await axios.get<ApiResponse>(`/api/accept-messages`);
+
+      setValue("acceptMessages", response.data.isAcceptingMessage);
+    } catch (error) {
+      console.error("Error in accepting messages!", error);
+
+      const axiosErorr = error as AxiosError<ApiResponse>;
+
+      toast.error("Something went wrong to accept message!");
+    } finally {
+      setIsSwitchLoading(false);
+    }
+  }, [setValue]);
+
+  const fetchMessages = useCallback(
+    async (refresh: boolean = false) => {
+      setLoading(true);
+      setIsSwitchLoading(false);
+      try {
+        const response = await axios.get<ApiResponse>("/api/get-message");
+        console.log(response.data.messages);
+        setMessages(response.data.messages || []);
+        if (refresh) {
+          toast.success("Refreshed messages...");
+        }
+      } catch (error) {
+        const axiosError = error as AxiosError<ApiResponse>;
+        toast.error("Failed to get refreshed messages!");
+      } finally {
+        setLoading(false);
+        setIsSwitchLoading(false);
+      }
+    },
+    [setLoading, setMessages]
+  );
+
+  // Fetch initial state from the server
+  useEffect(() => {
+    if (!session || !session.user) return;
+
+    fetchMessages();
+
+    fetchAcceptMessage();
+  }, [session, setValue, toast, fetchAcceptMessage, fetchMessages]);
+
+  // Handle switch change
+  const handleSwitchChange = async () => {
+    try {
+      const response = await axios.post<ApiResponse>("/api/accept-messages", {
+        acceptMessages: !acceptMessages,
+      });
+      setValue("acceptMessages", !acceptMessages);
+      toast.success("Message accepting status updated successfully...");
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast.error("Failed update message accepting status, please try again!");
+    }
+  };
+
+  if (!session || !session.user) {
+    return (
+      <div>
+        Please login to view dashboard, <Link href="/sing-in">Sign in</Link>
+      </div>
+    );
+  }
+
+  const { username } = session.user as User;
+
+  const baseUrl = `${window.location.protocol}//${window.location.host}`;
+  const profileUrl = `${baseUrl}/u/${username}`;
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(profileUrl);
+    toast.success("Link copied to clipboard.");
+  };
+
+  return (
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
+
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{" "}
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            className="input input-bordered w-full p-2 mr-2"
+          />
+          <Button onClick={copyToClipboard}>Copy</Button>
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <Switch
+          {...register("acceptMessages")}
+          checked={acceptMessages}
+          onCheckedChange={handleSwitchChange}
+          disabled={isSwitchLoading}
+        />
+        <span className="ml-2">
+          Accept Messages: {acceptMessages ? "On" : "Off"}
+        </span>
+      </div>
+      <Separator />
+
+      <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true);
+        }}
+      >
+        {loading ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <RefreshCcw className="h-4 w-4" />
+        )}
+      </Button>
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {messages.length > 0 ? (
+          messages.map((message, index) => (
+            <MessageCard
+              key={message._id as string}
+              message={message}
+              onMessageDelete={handleDeleteMessage}
+            />
+          ))
+        ) : (
+          <p>No messages to display.</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -3,6 +3,6 @@ import { Message } from "@/model/User";
 export interface ApiResponse {
   success: boolean;
   message: string;
-  isAcceptingMessages?: boolean;
+  isAcceptingMessage?: boolean;
   messages?: Array<Message>;
 }
